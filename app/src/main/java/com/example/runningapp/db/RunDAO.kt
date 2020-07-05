2c231a3be350da53862aad790a7fbf4c7a7629be
Kartik Bhargava <bhargava.kartik07@gmail.com>
2020-07-05 18:35:24 +0530

    MapView Lifecycle Done. Architectural Skeleton Done. Dagger-Hilt Setup Done.

--- app/src/main/java/com/example/runningapp/db/RunDAO.kt
+++ app/src/main/java/com/example/runningapp/db/RunDAO.kt
@@ -9,7 +9,7 @@ interface RunDAO {
     suspend fun insertRun(run: Run)
 
     @Delete
-    suspend fun deleteRun(run : Run)
+    suspend fun deleteRun(run: Run)
 
     @Query("SELECT * FROM `running_table ` ORDER BY timeStamp DESC")
     fun getAllRunsSortedByDate(): LiveData<List<Run>>
@@ -27,15 +27,15 @@ interface RunDAO {
     fun getAllRunsSortedBYDistance(): LiveData<List<Run>>
 
     @Query("SELECT SUM(timeInMillis) FROM `running_table ` ")
-    fun getTotalTimeInMillis() : LiveData<Long>
+    fun getTotalTimeInMillis(): LiveData<Long>
 
     @Query("SELECT SUM(caloriesBurned) FROM `running_table ` ")
-    fun getTotalCaloriesBurned() : LiveData<Int>
+    fun getTotalCaloriesBurned(): LiveData<Int>
 
     @Query("SELECT SUM(distanceInMeters) FROM `running_table ` ")
-    fun getTotalDistance() : LiveData<Int>
+    fun getTotalDistance(): LiveData<Int>
 
     @Query("SELECT AVG(avgSpeedInKMH) FROM `running_table ` ")
-    fun getTotalAvgSpeed() : LiveData<Float>
+    fun getTotalAvgSpeed(): LiveData<Float>
 
 }
