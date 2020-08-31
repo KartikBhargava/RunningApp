f104b9c7e1099fdd6d9e6205548f848d411fcc12
KartikBhargava <bhargava.kartik07@gmail.com>
2020-08-31 18:19:31 +0530

    Changes

--- app/src/main/java/com/example/runningapp/other/Constants.kt
+++ app/src/main/java/com/example/runningapp/other/Constants.kt
@@ -1,5 +1,7 @@
 package com.example.runningapp.other
 
+import android.graphics.Color
+
 object Constants {
 
     const val RUNNING_DATABASE_NAME = "running_db"
@@ -13,4 +15,7 @@ object Constants {
     const val FASTEST_LOCATION_INTERVAL = 2000L
     const val NOTIFICATION_CHANNEL_NAME = "Tracking"
     const val NOTIFICATION_ID = 1
+    const val POLYLINE_COLOR = Color.RED
+    const val POLYLINE_WIDTH = 8f
+    const val MAP_ZOOM = 15f
 }
--- app/src/main/java/com/example/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/TrackingFragment.kt
@@ -9,9 +9,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.runningapp.R
 import com.example.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.runningapp.other.Constants.MAP_ZOOM
+import com.example.runningapp.other.Constants.POLYLINE_COLOR
+import com.example.runningapp.other.Constants.POLYLINE_WIDTH
+import com.example.runningapp.services.PolyLine
+import com.example.runningapp.services.PolyLines
 import com.example.runningapp.services.TrackingService
 import com.example.runningapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
@@ -21,6 +29,9 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     private val viewModel: MainViewModel by viewModels()
 
     private var map: GoogleMap? = null
+
+    private var isTracking = false
+    private var pathPoints = mutableListOf<PolyLine>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,6 +43,47 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
             map = it
         }
     }
+
+    private fun updateTracking(isTracking:Boolean) {
+        this.isTracking = isTracking
+        
+    }
+
+    private fun addLatestPolyLine() {
+        if(pathPoints.isNotEmpty() && pathPoints.last().size > 1){
+            val preLastLatLng = pathPoints.last()[pathPoints.last().size-2]
+            val lastLatLng = pathPoints.last().last()
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .add(preLastLatLng)
+                .add(lastLatLng)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun moveCameraToUser() {
+        if(pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()){
+            map?.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    pathPoints.last().last(),
+                    MAP_ZOOM
+                )
+            )
+        }
+    }
+
+    private fun addAllPolyLines() {
+        for(polyline in pathPoints){
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .addAll(polyline)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+
     private fun sendCommandToService(action:String)=
         Intent(requireContext(), TrackingService::class.java).also{
             it.action = action
@@ -41,6 +93,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     override fun onResume() {
         super.onResume()
         mapView?.onResume()
+        addLatestPolyLine()
     }
 
     override fun onStart() {
