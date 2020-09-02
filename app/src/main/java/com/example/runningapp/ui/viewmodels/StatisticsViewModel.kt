8019ff04864070258cf12f70f11f5b60d11b6d0b
KartikBhargava <bhargava.kartik07@gmail.com>
2020-09-03 01:03:34 +0530

    Statistics part done

--- app/src/main/java/com/example/runningapp/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/example/runningapp/ui/viewmodels/StatisticsViewModel.kt
@@ -7,4 +7,12 @@ import com.example.runningapp.repositories.MainRepository
 class StatisticsViewModel @ViewModelInject constructor(
     private val mainRepository: MainRepository
 ) :ViewModel() {
+
+    val totalTimeRun  = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalAvgSpeed = mainRepository.getTotalAvgSpeed()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+
 }
